6775e641a2071abefff5849941e9d4d076ef2c73
Sam Eff <HeySamEff@gmail.com>
2020-08-03 11:57:54 -0700

    created posts model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_03_175627) do
+ActiveRecord::Schema.define(version: 2020_08_03_183436) do
+
+  create_table "posts", force: :cascade do |t|
+    t.string "title"
+    t.text "body"
+    t.integer "user_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
